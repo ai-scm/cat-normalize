2fe5cfa24015753a7561cbd1d93d5e244f7e0380
julian rincon <julian.rincon@blend360.com>
2026-02-03 15:45:29 -0500

    setting schedule expression as optional

--- infrastructure/lib/configs/test-etl.config.ts
+++ infrastructure/lib/configs/test-etl.config.ts
@@ -33,7 +33,6 @@ export const testEtlConfig: EtlConfig = {
     dynamoDbTableName: 'cat-dev-catia-conversations-table',
     outputPrefix: 'reports/feedbacks/',
     timeoutMinutes: 5,
-    scheduleExpression: 'rate(1 days)'
   },
   
   // Catalog Configuration
