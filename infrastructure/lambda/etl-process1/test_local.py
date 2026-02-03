35af39ad6289c2977ff65e463ed91375e82a7213
julian rincon <julian.rincon@blend360.com>
2026-02-03 15:21:51 -0500

    deleting local test files

--- infrastructure/lambda/etl-process1/test_local.py
+++
@@ -1,7 +0,0 @@
-# test_local.py
-import os
-os.environ['DYNAMODB_TABLE_NAME'] = 'cat-prod-catia-conversations-table'
-os.environ['S3_BUCKET_NAME'] = 'cat-test-normalize-reports'
-
-from feedback_analysis import lambda_handler
-response = lambda_handler({}, None)
